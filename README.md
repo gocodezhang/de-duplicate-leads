fb9fab34a88fc71e95aab445f69be1be8a0852e3
Gaoyuan Zhang <zgy25483387@gmail.com>
2024-12-05 22:18:06 -0800

    update comment and README

--- README.md
+++ README.md
@@ -1,2 +1,85 @@
 # de-duplicate-leads
-A exercise to de-duplicate leads data
+
+<!-- TABLE OF CONTENTS -->
+<details>
+  <summary>Table of Contents</summary>
+  <ol>
+    <li>
+      <a href="#how-to-run-the-program">How To Run The Program</a>
+      <ul>
+        <li>Approach #1 - Run the program in CLI interactively</li>
+        <li>Approach #2 - Run the test file in CLI</li>
+      </ul>
+    </li>
+    <li><a href="#deduplicate-algorithm-walkthrough">De-duplicate Algorithm Walkthrough</a></li>
+    <li>
+      <a href="#space-and-time-complexity">Space And Time Complexity</a>
+      <ul>
+        <li><a href="#other-considerations">Other Considerations</a></li>
+      </ul>
+    </li>
+  </ol>
+</details>
+
+## How To Run The Program
+
+**Note:** Please make sure install `Node.js` with version >= `v22.1.0` before proceed. See this link to install `Node.js`.
+
+### Approach #1 - Run the program in CLI interactively
+
+- Run below command to open Node CLI environment
+
+```sh
+npm run initalize
+```
+
+- The CLI environment has been loaded with function `deDuplicateStable`. Please call the function to run de-duplicate process and note below as an example
+
+```sh
+> const data = {
+  leads: [
+     {
+       _id: "jkj238238jdsnfsj23",
+       email: "foo@bar.com",
+       firstName: "John",
+       lastName: "Smith",
+       address: "123 Street St",
+       entryDate: "2014-05-07T17:30:20+00:00",
+     }
+    ]
+  }
+> deDuplicateStable(data)
+```
+
+### Approach #2 - Run the test file in CLI
+
+- Run below command to run test cases
+
+```sh
+npm run test
+```
+
+- You can update test cases directly in `test.js`
+
+## Deduplicate Algorithm Walkthrough
+
+- **Step 1** - Create a `keyMap` where `key = _id or email` and `value = Array of records`. Then add records with the same `_id` or `email` into the corresponding array
+- **Step 2** - Sort every array in `keyMap` so that the last record is the most preferrable based on rules
+- **Step 3** - Reconcile sorted arrays in the `keyMap` (e.g A record is the most preferrable under `_id = 2` but its `email = foo@bar.com` is duplicated with another record)
+- **Step 4** - Build the final output and print change log in the console based on Reconciled `keyMap`
+
+## Space And Time Complexity
+
+- Time Complexity: `O(nlogn)`
+  - While creating `keykeyMap` and reconciling `keyMap` should be in `O(n)`, but sort all arrys in `keyMap` could take up to `O(nlogn)`. (e.g when all records have same `_id`)
+  - For reconciling `keyMap`, while there is a nested `while` loop, the `while` loop will at most run `O(2n)` for the entire outer loop. So it is a `O(n)` operation.
+- Space Complexity: `O(n)`
+
+### Other Considerations
+
+1. `Array.sort` is a stable sort in javascript which maintain the original order of records if two records are equal. Hence `customSort` function only need to sort based on `entryDate`
+2. When adding records into `keyMap`, an extra property is added to store the `index` in the original array. So that the record original position can be accessed in constant time.
+   - Most importantly, `keyMap` store "entire change logs" **where in every array the last record is the final reconciled one and all previous records are ones converted into the final reconciled one**
+   - With `index`, a reversal can be performed
+3. There could be record A which can be reconciled into two records (B with same `_id` and C with same `email`). In the current algorithm, the record with the same `_id` (e.g B) is proritized when creating change logs.
+4. For "step 4 - build the final output", it could be more time efficient to build directly via `keyMap` given some records could be reconciled. But choosing to iterate through the original array in order to preserve the order in the output
--- solution/deDuplicate.js
+++ solution/deDuplicate.js
@@ -3,9 +3,9 @@ const emailPrefix = "email";
 
 function deDuplicateStable(data) {
   const { leads } = data;
-  // go through the data and find duplicate under the same id and email
+  // Step 1 - go through the leads and find duplicate under the same id and email
   const keyMap = new Map();
-
+  // for each record, add the record under array with same id as well as array with same email
   for (let i = 0; i < leads.length; i++) {
     const lead = leads[i];
     const { _id, email } = lead;
@@ -26,31 +26,37 @@ function deDuplicateStable(data) {
     arrayWithSameEmail.push(leadWithIndex);
   }
 
-  // sort all arrays in keyMap
+  // Step 2 - sort all arrays in keyMap
   for (const records of keyMap.values()) {
     records.sort(customSort);
   }
 
-  // cleanup
+  // Step 3 - Reconcile the sorted arrays
 
-  // create a exists array to store if the lead got removed in same id array or same email array
+  // create a array to store info about whether the record exists in same id array and same email array
   const exists = new Array(leads.length);
   for (let i = 0; i < exists.length; i++) {
-    const arr = [1, 1];
+    const arr = [1, 1]; // arr = [isExistSameIdArray, isExistSameEmailArray]
     exists[i] = arr;
   }
   for (const [key, records] of keyMap) {
+    // check if records is sameIdArray or sameEmailArray
     const isIdKey = key.substring(0, 5) === idPrefix ? true : false;
     let last = records[records.length - 1];
+    // for last record, find the corresponding array with the other key
+    // e.g if records is sameIdArray, find sameEmailArray. vice versa
     let recordsWithSameIdenity = isIdKey
       ? keyMap.get(`${emailPrefix}${last.lead.email}`)
       : keyMap.get(`${idPrefix}${last.lead._id}`);
 
+    // if last record is duplicated based on the other key, it cannot be the final reconciled record
+    // hence remove it from records
     while (
       last &&
       recordsWithSameIdenity.length &&
       last !== recordsWithSameIdenity[recordsWithSameIdenity.length - 1]
     ) {
+      // update exists array when remove element from records
       const { index } = records.pop();
       const exist = exists[index];
       if (isIdKey) {
@@ -58,6 +64,7 @@ function deDuplicateStable(data) {
       } else {
         exist[1] = 0;
       }
+
       last = records.length ? records[records.length - 1] : null;
       if (last !== null) {
         recordsWithSameIdenity = isIdKey
@@ -67,12 +74,15 @@ function deDuplicateStable(data) {
     }
   }
 
+  // Step 4 - build final output and print change logs
   return buildDeduplicatedLeads(leads, keyMap, exists);
 }
 
 function buildDeduplicatedLeads(leads, keyMap, exists) {
   // build result
   const result = [];
+
+  // use a set to track records added
   const set = new Set();
 
   // go through the leads instead of keyMap to maintain the original order of records
@@ -82,25 +92,30 @@ function buildDeduplicatedLeads(leads, keyMap, exists) {
     const leadsWithSameId = keyMap.get(`${idPrefix}${lead._id}`);
     const leadsWithSameEmail = keyMap.get(`${emailPrefix}${lead.email}`);
 
-    const isReconcliedRecord =
+    const isReconciledRecord =
       (leadsWithSameId.length &&
         lead === leadsWithSameId[leadsWithSameId.length - 1].lead) ||
       (leadsWithSameEmail.length &&
         lead === leadsWithSameEmail[leadsWithSameEmail.length - 1].lead);
 
-    if (isReconcliedRecord) {
+    if (isReconciledRecord) {
+      // add the record into result if it is the final reconciled record
+      // and it has not been added
       if (!set.has(_id) && !set.has(email)) {
         result.push(lead);
         set.add(_id);
         set.add(email);
       }
     } else {
+      // if it is not the final reconciled record, find its reconciled record and print change log
+
       const exist = exists[i];
       // target can exist in both but proritize chooing records with same id
       const target = exist[0]
         ? leadsWithSameId[leadsWithSameId.length - 1]
         : leadsWithSameEmail[leadsWithSameEmail.length - 1];
 
+      // build log
       buildLogAndPrint(lead, target.lead);
     }
   }
@@ -132,6 +147,7 @@ function buildLogAndPrint(source, target) {
     "\n"
   )}\n`;
 
+  // print change log in console
   console.log(log);
 }
 
